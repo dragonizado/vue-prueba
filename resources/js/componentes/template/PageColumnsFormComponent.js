0deb79b1c3764d5b0fb60f17cea6506777a892c4
Jesus Francisco <jesusfrancisco@hotmail.es>
2020-03-20 15:29:19 -0500

    Subiendo cambios

--- resources/js/componentes/template/PageColumnsFormComponent.js
+++ resources/js/componentes/template/PageColumnsFormComponent.js
@@ -7,7 +7,19 @@ Vue.component('page-columns-form-component',{
             const lista = this.$el;
             Sortable.create(lista,{
                 animation:150,
-                group:  'display-columns' 
+                group:  'display-columns' ,
+                store: {
+                    // Guardar el orden de la lista
+                    set: (sortable) => {
+                        const orden = sortable.toArray();
+                        localStorage.setItem(sortable.options.group.name, orden.join('|'));
+                    },
+                    // obtener el orden de la lista
+                    get: (sortable) => {
+                        const orden = localStorage.getItem(sortable.options.group.name)
+                        return orden ? orden.split('|') : [];
+                    }
+                }
             })
         },
     props:['columnsW','idColumn']
